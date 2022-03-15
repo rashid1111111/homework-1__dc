d6323347234e4b17287ef0d3306a52cf685d4c99
rashid1111111 <rashidbek0309@gmail.com>
2022-03-15 12:58:38 +0500

    finish

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-let obj = []
-let newObj = JSON.parse(localStorage.getItem("myData"))
-obj = obj.concat(newObj)
-const Add = () => {
-    let elInput = document.querySelector(".form-input")
-    obj.push({name: elInput.value})
-    localStorage.setItem("myData", JSON.stringify(obj))
-    Initial()
-    elInput.value = ''
-}
-Initial()
+const form = document.querySelector('.form')
+const input = document.querySelector('.form-input')
+const list = document.querySelector('.list')
 
-function Initial() {
-    filtered = obj.filter(item => {
-        return item !== null
-    })
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const elLi = document.createElement('li')
+    elLi.textContent = input.value
+    list.appendChild(elLi)
 
-
-
-    let names = document.querySelector(".names")
-
-    names.innerHTML = filtered.map((item, index) => {
-        let btn = `<button onclick="Delete(${index})">del</button>`
-        return `<div>${item.name} ${btn}</div>`
-    }
-    )
-
-    // names.innerHTML = ism
-    // console.log(newArr)
-
-}
-function Delete(num){
-    filtered.splice(num, 1)
-}
+    input.value = ''
+})
